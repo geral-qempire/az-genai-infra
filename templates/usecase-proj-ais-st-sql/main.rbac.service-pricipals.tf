f920bca04aca02d7fbdb85cc5b77941cb7574aa1
qempire <geral.qempire@gmail.com>
2025-09-03 15:30:56 +0100

    Update Group rbac

--- templates/usecase-proj-ais-st-sql/main.rbac.service-pricipals.tf
+++ templates/usecase-proj-ais-st-sql/main.rbac.service-pricipals.tf
@@ -4,6 +4,21 @@
 
 locals {
   service_principal_rbac = merge(
+    var.sp_cicd_id != null && var.sp_cicd_id != "" ? {
+      # SP_CICD → Storage Account
+      cicd_storage_blob_contrib = {
+        principal_id         = var.sp_cicd_id
+        scope_id             = module.storage_account.storage_account_id
+        role_definition_name = "Storage Blob Data Contributor"
+        principal_type       = "ServicePrincipal"
+      }
+      cicd_storage_reader = {
+        principal_id         = var.sp_cicd_id
+        scope_id             = module.storage_account.storage_account_id
+        role_definition_name = "Reader"
+        principal_type       = "ServicePrincipal"
+      }
+    } : {},
     var.sp_app_id != null && var.sp_app_id != "" ? {
       # SP_APP → AI Services
       app_ai_services_openai_user = {
